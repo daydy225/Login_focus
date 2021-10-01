32a18d1f006f362eea965ef54eaac4b32885b5f3
daydy225 <dabelydadi@gmail.com>
2021-10-01 07:22:48 +0000

    Login Focus save 2

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,4 @@
-     
+     const btn_click = document.getElementById("btn")
+     let email = document.getElementById("email");
+     let passwd = document.getElementById("pwrd")
+     let cpasswd = document.getElementById("cpwrd")
