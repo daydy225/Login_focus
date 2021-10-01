6e5ca5ff8ad86d35150f1231e2cf3deef22ac23b
daydy225 <dabelydadi@gmail.com>
2021-10-01 12:31:12 +0000

    Login Focus
    MAJ javascript

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,23 @@
-     const btn_click = document.getElementById("btn")
-     let email = document.getElementById("email");
-     let passwd = document.getElementById("pwrd")
-     let cpasswd = document.getElementById("cpwrd")
+      
+
+     function verif() {
+        let nom = document.getElementById("name").value
+        let email = document.getElementById("email").value
+        let passwd = document.getElementById("pwrd").value
+        let cpasswd = document.getElementById("cpwrd").value
+        if (nom.length <= 60) {
+            let emailptn =  /\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/
+            
+              if (emailptn.test(email)) {
+                 if (passwd !== cpasswd) {
+                     alert("Votre mot de passe ne correspont pas")
+                 } else {
+                     alert("Le mot de passe est correct")
+                 }
+              } else {
+                  alert("Email n'est pas au bon format")
+              }
+        } else {
+             alert ("Votre Nom doit avoir au plus 60 caracteres")
+        }
+     }
